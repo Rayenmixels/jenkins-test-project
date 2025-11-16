e685868a6c34367721c8d69183e6743b542288b0
Mohamed Rayen Jlassi <jlassi.mohamedrayen@esprit.tn>
2025-11-16 17:18:42 +0100

    third jenkins test automation

--- src/main/java/tn/esprit/spring/entities/User.java
+++ src/main/java/tn/esprit/spring/entities/User.java
@@ -20,7 +20,7 @@ public class User implements Serializable {
 
 
 	private static final long serialVersionUID = 1L;
-
+///  second test jenkins
 	@Id
 	@GeneratedValue (strategy = GenerationType.IDENTITY)
 	private Long id;   
